319d47dbcf12b38e342902ea483a99ce29ed3d9c
luissancar <luissancar@yahoo.es>
2016-10-16 13:23:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/luissancar/actionbarbasica/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.luissancar.actionbarbasica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuopciones, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.opcion1) {
+            Toast.makeText(this,"Se seleccionó la primer opción",Toast.LENGTH_LONG).show();
+        }
+        if (id==R.id.opcion2) {
+            Toast.makeText(this,"Se seleccionó la segunda opción",Toast.LENGTH_LONG).show();
+        }
+        if (id==R.id.opcion3) {
+            Toast.makeText(this,"Se seleccionó la tercer opción", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
